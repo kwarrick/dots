21ccb0fff6e7520b87ef1195fd4eed41862cd008
Kevin Warrick <kevinjwarrick@gmail.com>
2013-02-20 02:45:30 -0500

    remove link.sh

--- link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-files=".zshrc .zsh .vimrc .vim .gitconfig .screenrc .gdbinit"
-dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-for file in $files; do
-  mv -nv ~/$file{,.old} &&
-  ln -sv "$(find $dir -name $file)" ~/$file
-done
