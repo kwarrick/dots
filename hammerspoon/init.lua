30c4d15a7eabdd3a95fb9fdf934af1df6608652c
Kevin Warrick <kevinjwarrick@gmail.com>
2017-07-06 17:47:14 -0400

    Remove kwm and ubersicht

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,95 +1,9 @@
 --------------------------------------------------------------------------------
--- KWM integration
+-- Window Navigation
 --------------------------------------------------------------------------------
-function kwmc_(args)
-    hs.task.new('/usr/local/bin/kwmc', nil, args):start()
-end
-
-function kwmc(cmd)
-  local args = hs.fnutils.split(cmd, ' ', nil, true)
-  return (function() kwmc_(args) end)
-end
-
---------------------------------------------------------------------------------
--- Window navigation
---------------------------------------------------------------------------------
-hs.hotkey.bind({'cmd', 'shift'}, 'j', kwmc('window -f next'))
-hs.hotkey.bind({'cmd', 'shift'}, 'k', kwmc('window -f prev'))
 
 -- Vim easy-motion for applications
 hs.hotkey.bind('alt', 'tab', hs.hints.windowHints)
-
---------------------------------------------------------------------------------
--- Window resizing
---------------------------------------------------------------------------------
-hs.hotkey.bind({'cmd', 'shift'}, 'm', kwmc('window -z fullscreen'))
-hs.hotkey.bind({'cmd', 'shift'}, 'f', kwmc('window -t focused'))
-hs.hotkey.bind({'cmd', 'shift'}, 'o', kwmc('window -m display next'))
-
--- Reproduce Awesome's resizing, which a fixed shortcut for respective
---    $ kwmc window -c reduce 0.05 focused
---    $ kwmc window -c expand 0.05 focused
--- will not affect.
-hs.hotkey.bind({'cmd', 'shift'}, 'h', function()
-  local window = hs.window.frontmostWindow()
-  local others = window:otherWindowsSameScreen()
-  local adjacent = window:windowsToEast(others, false, true)
-  local direction = (#adjacent) > 0 and 'east' or 'west'
-  local action = (#adjacent) > 0 and 'reduce' or 'expand'
-  kwmc_{'window', '-c', action, '0.01', direction}
-end)
-
-hs.hotkey.bind({'cmd', 'shift'}, 'l', function()
-  local window = hs.window.frontmostWindow()
-  local others = window:otherWindowsSameScreen()
-  local adjacent = window:windowsToEast(others, false, true)
-  local direction = (#adjacent) > 0 and 'east' or 'west'
-  local action = (#adjacent) > 0 and 'expand' or 'reduce'
-  kwmc_{'window', '-c', action, '0.01', direction}
-end)
-
---------------------------------------------------------------------------------
--- Window movement
---------------------------------------------------------------------------------
-hs.hotkey.bind({'cmd', 'shift'}, 'r', kwmc('tree rotate 90'))
-
-for i=1,6 do
-  -- Move window to space `i'
-  hs.hotkey.bind({'cmd', 'shift'}, '' .. i, kwmc('window -m space ' .. i))
-end
-
--- Padding and gaps.
-hs.hotkey.bind({'ctrl', 'alt'}, '-', kwmc('space -p increase all'))
-hs.hotkey.bind({'ctrl', 'alt'}, '=', kwmc('space -p decrease all'))
-hs.hotkey.bind({'ctrl', 'alt'}, '[', kwmc('space -g decrease all'))
-hs.hotkey.bind({'ctrl', 'alt'}, ']', kwmc('space -g increase all'))
-
--- Floating window movements
-function moveWindow(x, y, w, h)
-  return function()
-    local win = hs.window.focusedWindow()
-    local rect = hs.geometry.new(x, y, w, h)
-    win:moveToUnit(rect, 0) end
-end
-hs.hotkey.bind({'ctrl', 'alt'}, 'h', moveWindow(0, 0, 0.5, 1))
-hs.hotkey.bind({'ctrl', 'alt'}, 'j', moveWindow(0, 0.5, 1, 0.5))
-hs.hotkey.bind({'ctrl', 'alt'}, 'k', moveWindow(0, 0, 1, 0.5))
-hs.hotkey.bind({'ctrl', 'alt'}, 'l', moveWindow(0.5, 0, 0.5, 1))
-hs.hotkey.bind({'ctrl', 'alt'}, 'return', moveWindow(0, 0, 1, 1))
-
---------------------------------------------------------------------------------
--- Tiling modes
---------------------------------------------------------------------------------
-local modes = hs.fnutils.cycle{'bsp', 'float', 'monocle'}
-hs.hotkey.bind({'cmd', 'shift'}, 'space', function()
-  local mode = modes()
-  hs.alert.closeAll()
-  hs.alert.show(mode)
-  kwmc_{'space', '-t', mode}
-end)
-hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'j', kwmc('window -s next'))
-hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'k', kwmc('window -s prev'))
-hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'space', kwmc('window -c type toggle'))
 
 --------------------------------------------------------------------------------
 -- Application shortcuts
@@ -103,7 +17,7 @@ function spawnTerminal()
     iterm:selectMenuItem({'Shell', 'New Window'})
   end
 end
-hs.hotkey.bind({'cmd', 'shift'}, 'return', spawnTerminal)
+hs.hotkey.bind({'opt', 'shift'}, 'return', spawnTerminal)
 
 --------------------------------------------------------------------------------
 -- FN + HJKL for arrows
@@ -121,5 +35,15 @@ function fnArrows(event)
     end
   end
 end
-local tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, fnArrows)
+
+-- function tapHJKL()
+--   print("starting tapHJKL")
+--   local tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, fnArrows)
+--   tap:start()
+--   local isEnabled = (function () return tap:isEnabled() end)
+--   hs.timer.waitWhile(isEnabled, tapHJKL, 5)
+-- end
+-- tapHJKL()
+
+tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, fnArrows)
 tap:start()
